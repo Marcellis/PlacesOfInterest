87417beddd8706a61c047443048d93933a0a7389
Marco Marcellis <m.m.c.m.marcellis@hva.nl>
2017-11-02 13:46:32 +0100

    second commmit

--- app/src/main/java/com/example/marmm/placesofinterest/MainActivity.java
+++ app/src/main/java/com/example/marmm/placesofinterest/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.marmm.placesofinterest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<GeoObject> mGeoObjects = new ArrayList<>();
+
+        for (int i = 0; i < GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES.length; i++) {
+
+            mGeoObjects.add(new GeoObject(GeoObject.PRE_DEFINED_GEO_OBJECT_NAMES[i],
+                    GeoObject.PRE_DEFINED_GEO_OBJECT_IMAGE_IDS[i]));
+        }
+
+        RecyclerView mGeoRecyclerView =  findViewById(R.id.recyclerView);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2); //2 cells per row
+
+        mGeoRecyclerView.setLayoutManager(mLayoutManager);
+        GeoObjectAdapter mAdapter = new GeoObjectAdapter(this, mGeoObjects);
+        mGeoRecyclerView.setAdapter(mAdapter);
     }
+
+
+
 }
